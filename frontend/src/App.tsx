de5dd8317d7a87e8dcc47c84ffd18bce2d24e536
GLemes25 <gabriellemes924@gmail.com>
2024-07-01 21:35:12 -0400

    componentes estaticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
 
-function App() {
+const App = () => {
   return (
-    <div>
+    <>
+    <NavBar />
+    <div className="container"> 
       <h1 className="text-primary">Olá mundo!</h1>
+    <DataTable/>
     </div>
+     <Footer/>
+    </>
   ); 
 }
 
